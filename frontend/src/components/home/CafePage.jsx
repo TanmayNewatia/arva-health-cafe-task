239b3eb5be06a09c94cd3c166f924303e360822b
Tanmay Newatia <tr8497@srmist.edu.in>
2024-06-23 10:29:56 +0530

    feat: docker files added

--- frontend/src/components/home/CafePage.jsx
+++ frontend/src/components/home/CafePage.jsx
@@ -14,7 +14,7 @@ const CafePage = () => {
     fetch(`http://localhost:4000/api/cafes/${pathname[2]}`, requestOptions)
       .then(async (response) => { const data = await response.json(); setData(data); })
       .catch((error) => console.error(error));
-  }, [])
+  }, [pathname])
 
   return (
     <div className='mt-[100px] w-screen'>
@@ -28,7 +28,7 @@ const CafePage = () => {
               <p className='text-[#003B40] font-bold tracking-wider text-3xl'>{data.name}</p>
               <div className='text-md flex items-center gap-2'><img src="/assets/star.svg" alt="star" className='w-[15px] h-[15px]' /><span>{data.ratings[0]["score"]}</span><p className='text-[#B7C1C2]'>1,200 reviews</p></div>
               <p className='text-[#003B40] text-sm font-medium tracking-widest'>{data.description}</p>
-              <a href={`https://www.google.co.in/maps/place/${data.location.latitude}+${data.location.longitude}`} target='_blank'><FaLocationPin /></a>
+              <a href={`https://www.google.co.in/maps/place/${data.location.latitude}+${data.location.longitude}`} target='_blank' rel="noreferrer"><FaLocationPin /></a>
             </div>
           </div>
           <div className='flex flex-col items-start gap-4 py-16 w-1/2 min-w-[300px]'>
