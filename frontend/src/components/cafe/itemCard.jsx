72d36cbf4e05a86f05159917b6680fad40a06b37
Tanmay Newatia <tr8497@srmist.edu.in>
2024-06-23 07:53:59 +0530

    feat: data displayed on the frontend

--- frontend/src/components/cafe/itemCard.jsx
+++ frontend/src/components/cafe/itemCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { FaPlus } from "react-icons/fa6";
 
-const itemCard = () => {
+const itemCard = ({ items }) => {
     return (
-        <div className='w-11/12 mx-auto min-w-[300px] max-w-[600px] bg-[#EDF0EF] p-2 rounded-2xl'>
+        <div className='w-8/12 mx-auto min-w-[300px] max-w-[600px] bg-[#EDF0EF] p-2 rounded-2xl'>
             <div className='flex justify-end'>
                 <div className='bg-[#ffffff] p-2 rounded-full absolute w-[40px] h-[40px] -mt-[20px] -mr-[20px] flex justify-center items-center'>
                     <div className='bg-[#003B40] text-[#EDF0EF] p-2 rounded-full absolute w-[35px] h-[35px] flex justify-center items-center'>
@@ -11,12 +11,12 @@ const itemCard = () => {
                     </div>
                 </div>
             </div>
-            <div className='flex justify-evenly gap-4'>
-                <img src="/assets/item.svg" alt="Coffe" />
+            <div className='flex justify-evenly gap-4 flex-wrap'>
+                <img src={items.image} alt="Coffe" className='h-[150px] rounded-3xl object-contain object-center' />
                 <div className='flex flex-col justify-evenly'>
-                    <p className='text-[#003B40] font-bold tracking-wider'>Cafè mocha</p>
-                    <p className='text-[#003B40] text-md w-10/12'>A chocolate-flavored warm beverage that is a variant of a café latte</p>
-                    <p p className='text-[#003B40] text-sm font-medium tracking-widest'>$3.00</p>
+                    <p className='text-[#003B40] font-bold tracking-wider'>{items.name}</p>
+                    {/* <p className='text-[#003B40] text-md w-10/12'>A chocolate-flavored warm beverage that is a variant of a café latte</p> */}
+                    <p p className='text-[#003B40] text-md font-medium tracking-widest'>₹{items.price}</p>
                 </div>
             </div>
         </div>
--- frontend/src/components/home/Footer.jsx
+++ frontend/src/components/home/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='flex justify-center items-center w-screen flex-col gap-4 bg-[#EDF0EF]  tracking-wide font-medium text-md'>
-            <div className="flex justify-evenly w-11/12 py-8">
-                <div className='flex flex-col gap-4'>
+            <div className="flex justify-evenly w-11/12 py-8 flex-wrap gap-4">
+                <div className='flex flex-col gap-4 max-sm:min-w-[300px]'>
                     <img src="/logo.png" alt="Cafe" className='w-[75px]' />
                     <a href="https://www.google.co.in/maps/place/MG+ROAD+METRO+STATION/@12.9756295,77.6040478,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae17005c679be5:0x7fe614760a22fc8e!8m2!3d12.9756295!4d77.6066227!16s%2Fg%2F11v_9gwhsj?entry=ttu">
                         <p className='text-sm flex flex-col'><span className='font-semibold'>Address:</span>
@@ -15,7 +15,7 @@ const Footer = () => {
                         </p>
                     </a>
                 </div>
-                <ul>
+                <ul className='max-sm:min-w-[300px]'>
                     <li className='!font-semibold text-[#003B40] text-lg'>
                         Company
                     </li>
@@ -23,7 +23,7 @@ const Footer = () => {
                     <li>Career</li>
                     <li>Team</li>
                 </ul>
-                <div className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-4 max-sm:min-w-[300px]'>
                     <ul>
                         <li className='font-semibold text-[#003B40] text-lg'>
                             Contact Us
@@ -39,7 +39,7 @@ const Footer = () => {
                         <li>Cookie Policy</li>
                     </ul>
                 </div>
-                <ul>
+                <ul className='max-sm:min-w-[300px]'>
                     <li className='font-semibold text-[#003B40] text-lg'>
                         Cities we serve:
                     </li>
